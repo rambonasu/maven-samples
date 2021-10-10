3fd861b25adbc762c130dfab9c5accf5fc59ea23
Sriram <sriram.bonasu@gmail.com>
2021-10-10 15:37:36 -0400

    changed for scm

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -32,8 +32,8 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+  public void greetShouldIncludeGreetingMessagelike() {
+    String someone = "World, Thanks you making me Happy for me";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
